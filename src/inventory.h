b0715e57b760359be9a0fdfbd3b8e08584f9fad3
cx384 <cx384@proton.me>
2025-02-01 14:12:38 +0100

    Fix overrideable hand ToolCapabilities and range

--- src/inventory.h
+++ src/inventory.h
@@ -102,18 +102,25 @@ struct ItemStack
 	}
 
 	// Get tool digging properties, or those of the hand if not a tool
+	// If not hand assumes default hand ""
 	const ToolCapabilities& getToolCapabilities(
-			const IItemDefManager *itemdef) const
+			const IItemDefManager *itemdef, const ItemStack* hand = nullptr) const
 	{
-		const ToolCapabilities *item_cap =
-			itemdef->get(name).tool_capabilities;
+		const ToolCapabilities *item_cap = itemdef->get(name).tool_capabilities;
 
-		if (item_cap == NULL)
-			// Fall back to the hand's tool capabilities
-			item_cap = itemdef->get("").tool_capabilities;
-
-		assert(item_cap != NULL);
-		return metadata.getToolCapabilities(*item_cap); // Check for override
+		if (item_cap != nullptr) {
+			return metadata.getToolCapabilities(*item_cap); // Check for override
+		} else { // Fall back to the hand's tool capabilities
+			if (hand != nullptr) {
+				item_cap = itemdef->get(hand->name).tool_capabilities;
+				assert(item_cap != nullptr);
+				return hand->metadata.getToolCapabilities(*item_cap);
+			} else {
+				item_cap = itemdef->get("").tool_capabilities;
+				assert(item_cap != nullptr);
+				return *item_cap;
+			}
+		}
 	}
 
 	const std::optional<WearBarParams> &getWearBarParams(
